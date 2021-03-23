5ac4b1e7e915f68dda707a30e5962776c2407e39
Kuddleman <edl1155@gmail.com>
2021-03-22 21:53:41 -0700

    add maptest

--- src/ListTester.java
+++ src/ListTester.java
@@ -7,11 +7,37 @@ public class ListTester {
         List<EmergencyShelterWaitlist> shelterList = new ArrayList<>();
         fillList(shelterList);
 
+        // Two interesting queries:
+        //1. How many people are on the shelterList?
+        System.out.println("There are " + shelterList.size() + " people on tonight's waitlist.");
+
+        //2. How many people on the list were born in 1960?
+        int count = 0;
+        for ( EmergencyShelterWaitlist obj : shelterList) {
+            if (obj.getDateOfBirth().substring(6).equals("1960")) {
+                count ++;
+            }
+        }
+
+        System.out.println("The number of people on the waitlist who were born in 1960 is: " + count);
+
+
+        // Create Map
+        //  We'll reassign the position num to begin with 1001
+        int newId = 1001;
+        Map<String, Object > map = new HashMap<>();
+        for ( EmergencyShelterWaitlist obj : shelterList){
+                String idStr = Integer.toString(newId);
+                obj.setPosition(idStr);
+                map.put(idStr, obj);
+                newId ++;
+        }
+
 
     }
 
     private static void fillList(List<EmergencyShelterWaitlist> list) {
-        try(Scanner fileScan = new Scanner(new FileReader(new File("HSH_90-day_emergency_shelter_waitlist.csv")))) {
+        try(Scanner fileScan = new Scanner(new FileReader(new File("src/shelterList.csv")))) {
             String line = fileScan.nextLine();
 
             while(fileScan.hasNext()) {
@@ -35,6 +61,7 @@ public class ListTester {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
     }
+
+
 }
